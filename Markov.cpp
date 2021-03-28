a8b15cf48a18af9a76dace0cfd41a391e21dc4b1
Anno2003 <idsale2002@yahoo.com>
2021-03-28 19:40:17 +0700

    tried extracting each word from input.txt to generate sentences

--- Markov.cpp
+++ Markov.cpp
@@ -4,36 +4,37 @@
 #include <map>
 #include <time.h>
 #include <fstream>
+#include <algorithm>
 using namespace std;
 
-
+string get_last_word(const string& s) {
+  auto index = s.find_last_of(' ');
+  return s.substr(++index);
+}
 int main(int argc,char* argv[]){
-	string txt;
+	srand (time(NULL));
 	ifstream input("input.txt");
 	if(!input.is_open()){printf("cannot load input.txt");return 0;}
-	for(string line;getline(input,line);){
-		txt+=line;
+	
+	map<string,vector<string>> ngram;
+	vector<string> words;
+	for(string line;getline(input,line,' ');){
+		line.erase(remove(line.begin(),line.end(),'\n'),line.end());//remove newline from word
+		words.push_back(line);
+	}
+	for(int i=0;i<(int)words.size();i++){
+		ngram[words[i]].push_back(words[i+1]);
 	}
 	
-	srand (time(NULL));
-	
-	int order=6;//order of grams
-	
-	map<string,vector<char>> ngram;
-	for(int i=0;i<((int)txt.length() - order);i++){
-		string gram =txt.substr(i,order);
-		
-		ngram[gram].push_back(txt.at(i+order));
-	}
-	
-	string currentGram = txt.substr(rand()%txt.size()-order,order);//Take random for beggining
+	string currentGram = words[(int)rand()%words.size()];//Take random word for beggining
 	string result = currentGram;
-	for(int i=0;i< 100;i++){
-		vector<char> possibilities=ngram[currentGram];
-		char next= possibilities[(int)rand()%possibilities.size()];
-		result+=next;
-		currentGram=result.substr(result.length()-order,order);
+	while(result.substr(result.length()-1)!="."){
+		vector<string> possibilities=ngram[currentGram];
+		string next= possibilities[(int)rand()%possibilities.size()];
+		result+=" "+next;
+		currentGram=get_last_word(result);
 	}
 	cout<<result<<endl;
+	
 	return 0;
 }
