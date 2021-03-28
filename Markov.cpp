2ff92c4ccdd404b0d8c29b8795539400446bbdd0
Anno2003 <idsale2002@yahoo.com>
2021-03-28 23:21:05 +0700

    fixed word not spacing correctly

--- Markov.cpp
+++ Markov.cpp
@@ -4,13 +4,15 @@
 #include <map>
 #include <time.h>
 #include <fstream>
-#include <algorithm>
+#include <sstream>
 using namespace std;
 
 string get_last_word(const string& s) {
   auto index = s.find_last_of(' ');
   return s.substr(++index);
 }
+
+
 int main(int argc,char* argv[]){
 	srand (time(NULL));
 	ifstream input("input.txt");
@@ -18,17 +20,21 @@ int main(int argc,char* argv[]){
 	
 	map<string,vector<string>> ngram;
 	vector<string> words;
-	for(string line;getline(input,line,' ');){
-		line.erase(remove(line.begin(),line.end(),'\n'),line.end());//remove newline from word
-		words.push_back(line);
+	
+	for(string line;getline(input,line);){//turn each line to stringstream
+		stringstream iss(line);
+		for(string word;getline(iss,word,' ');){//take each word 
+			words.push_back(word);
+		}
 	}
+	
 	for(int i=0;i<(int)words.size();i++){
-		ngram[words[i]].push_back(words[i+1]);
+		ngram[words[i]].push_back(words[i+1]);//map word with all possibilities of adjacent word;
 	}
 	
 	string currentGram = words[(int)rand()%words.size()];//Take random word for beggining
 	string result = currentGram;
-	while(result.substr(result.length()-1)!="."){
+	while((result.substr(result.length()-1)!=".")||(result.length()<100)){
 		vector<string> possibilities=ngram[currentGram];
 		string next= possibilities[(int)rand()%possibilities.size()];
 		result+=" "+next;
